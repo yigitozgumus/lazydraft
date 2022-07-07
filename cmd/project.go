bc1a4c940e11ce93cefe21768c7d3d839c69c21b
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-07-07 19:33:10 +0300

    Convert the application to cli

---
+++ cmd/project.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"lazy-publish/lazypublish"
+	"log"
+)
+
+func registerProjectCommand() *cli.Command {
+	command := cli.Command{
+		Name:    "project",
+		Aliases: []string{"p"},
+		Usage:   "Track your projects",
+		Subcommands: []*cli.Command{
+			registerProjectListCommand(),
+			registerGetActiveProjectCommand(),
+			registerChangeActiveProjectCommand(),
+		},
+	}
+	return &command
+}
+
+func registerProjectListCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "list",
+		Aliases: []string{"l"},
+		Usage:   "List your current projects",
+		Action: func(context *cli.Context) error {
+			pc, err := lazypublish.GetProjectConfig()
+			if err != nil {
+				return err
+			}
+			projectList := lazypublish.GetProjectList(*pc)
+			projectNames := projectList.GetProjectNames()
+			activeProject, _ := projectList.GetActiveProject()
+			activeProjectName := activeProject.Name
+			fmt.Println("\nCurrent Project List")
+			for index, name := range projectNames {
+				activeOutput := ""
+				if name == activeProjectName {
+					activeOutput = "(active)"
+				}
+				fmt.Printf("  %d) %s %s\n", index+1, name, activeOutput)
+			}
+			return nil
+		},
+	}
+}
+
+func registerGetActiveProjectCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "active",
+		Aliases: []string{"a"},
+		Usage:   "Get your active project for draft management",
+		Action: func(context *cli.Context) error {
+			pc, err := lazypublish.GetProjectConfig()
+			if err != nil {
+				return err
+			}
+			projectList := lazypublish.GetProjectList(*pc)
+			activeProject, err := projectList.GetActiveProject()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("\nCurrent active project is %s\n", activeProject.Name)
+			return nil
+		},
+	}
+}
+
+func registerChangeActiveProjectCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "change",
+		Aliases: []string{"c"},
+		Usage:   "Change your active project for draft management",
+		Action: func(context *cli.Context) error {
+			return nil
+		},
+	}
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.18
 
 require (
 	github.com/otiai10/copy v1.7.0
+	github.com/urfave/cli/v2 v2.10.3
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
