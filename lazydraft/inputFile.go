106d47178de119094a696ec1762fac3f6b1e5696
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-07-14 10:50:20 +0300

    Update init command to handle empty settings file

--- lazydraft/inputFile.go
+++ lazydraft/inputFile.go
@@ -8,27 +8,30 @@ import (
 )
 
 const projectFileDoesNotExistsError = "\nprojects.yml file could not be found. See 'lazydraft help init'"
+const settingsFileDoesNotExistsError = "\nsettings.yml file could not be found. See 'lazydraft help init'"
 const projectFormatInvalidError = "\nprojects.yml file format is invalid. See 'lazydraft config'"
-const ConfigFileName = ".config/lazydraft/projects.yml"
+const settingsFormatInvalidError = "\nsettings.yml file format is invalid. See 'lazydraft config'"
+const ProjectDataFilePath = ".config/lazydraft/projects.yml"
 const ConfigFileDir = "lazydraft"
 const ConfigBaseDir = ".config"
 const userHomeDirectoryError = "user home directory cannot be retrieved"
-const configFilePathError = "lazydraft file path cannot be retrieved"
+const projectDataFilePathError = "projects.yml file path cannot be retrieved"
+const settingsFilePathError = "settings.yml file path cannot be retrieved"
 
 type InputFile struct {
 	Path string
 }
 
-type ProjectConfig struct {
+type ProjectPathList struct {
 	Data map[string]YamlFile
 }
 
-func (yf InputFile) readProjectConfig() (*ProjectConfig, error) {
+func (yf InputFile) readProjectPathList() (*ProjectPathList, error) {
 	data, err := ioutil.ReadFile(yf.Path)
 	if err != nil {
 		return nil, errors.New(projectFileDoesNotExistsError)
 	}
-	projectConfig := ProjectConfig{
+	projectConfig := ProjectPathList{
 		Data: make(map[string]YamlFile),
 	}
 	err = yaml.Unmarshal(data, &projectConfig.Data)
@@ -38,25 +41,25 @@ func (yf InputFile) readProjectConfig() (*ProjectConfig, error) {
 	return &projectConfig, nil
 }
 
-func GetProjectConfig() (*ProjectConfig, error) {
-	configPath, err := getConfigFilePath()
+func GetProjectListData() (*ProjectPathList, error) {
+	projectListPath, err := getProjectPathDataPath()
 	if err != nil {
-		return nil, errors.New(configFilePathError)
+		return nil, errors.New(projectDataFilePathError)
 	}
-	projectConfig, err := configPath.readProjectConfig()
+	projectConfig, err := projectListPath.readProjectPathList()
 	if err != nil {
 		return nil, err
 	}
 	return projectConfig, nil
 }
 
-func getConfigFilePath() (*InputFile, error) {
+func getProjectPathDataPath() (*InputFile, error) {
 	dirname, err := os.UserHomeDir()
 	if err != nil {
 		return nil, errors.New(userHomeDirectoryError)
 	}
 	configFile := InputFile{
-		Path: dirname + "/" + ConfigFileName,
+		Path: dirname + "/" + ProjectDataFilePath,
 	}
 	return &configFile, nil
 }
